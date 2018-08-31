c3f02dd2a9a26539429915584a601aa48043318a
yuan <htyuan@zhiniuxue.com>
2018-08-31 15:22:08 +0800

    装饰者：用委托替换继承

--- src/dp/decorate/DecodingNode.java
+++ src/dp/decorate/DecodingNode.java
@@ -3,14 +3,32 @@ package dp.decorate;
 /**
  * 解码node
  */
-public class DecodingNode extends StringNode{
+public class DecodingNode implements Node{
+    private Node delegate;
 
     public DecodingNode(String text) {
+        delegate = new StringNode();
+
         setText(text);
     }
 
+    @Override
+    public String toHtml() {
+        return delegate.toHtml();
+    }
+
     public String toPlainTextString() {
-        return Translate.decode(text);
+        return Translate.decode(delegate.toPlainTextString());
+    }
+
+    @Override
+    public void setText(String text) {
+        delegate.setText(text);
+    }
+
+    @Override
+    public String getText() {
+        return delegate.getText();
     }
 
     public boolean isShouldDecode() {
